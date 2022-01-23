a64c361eeff57cf09ea2a23e0cbd6b94dd686492
Kai Hui Ang <s10198138@connect.np.edu.sg>
2022-01-23 18:47:08 +0800

    Changes

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func CreateNewTutor(db *sql.DB, t Tutor) {
 	}
 }
 
-//Updating existing passenger information
+//Updating existing tutor information
 func UpdateTutor(db *sql.DB, t Tutor) {
 	query := fmt.Sprintf("UPDATE Tutors SET Name='%s', Description='%s' WHERE TutorID='%s'",
 		t.Name, t.Description, t.TutorID)
@@ -55,7 +55,8 @@ func UpdateTutor(db *sql.DB, t Tutor) {
 	}
 }
 
-//Updating existing passenger information
+/*
+//Updating existing tutor information
 func ViewTutor(db *sql.DB, t Tutor) {
 	query := fmt.Sprintf("SELECT * FROM Tutors WHERE TutorID='%s'",
 		t.TutorID, t.Name, t.Description)
@@ -63,7 +64,7 @@ func ViewTutor(db *sql.DB, t Tutor) {
 	if err != nil {
 		panic(err.Error())
 	}
-}
+}*/
 
 func DeleteTutor(db *sql.DB, t Tutor) {
 	query := fmt.Sprintf("DELETE FROM Tutors WHERE TutorID='%s'",
@@ -82,6 +83,7 @@ func ListTutors(db *sql.DB, t Tutor) {
 	}
 }
 
+/*
 //Searches tutor based on personal information
 func SearchTutors(db *sql.DB, t Tutor) {
 	query := fmt.Sprintf("SELECT * FROM Tutors WHERE TutorID='%s'",
@@ -92,17 +94,17 @@ func SearchTutors(db *sql.DB, t Tutor) {
 	}
 }
 
-/*
+
 //Tutor using mobile phone number to login
-func TutorLogin(db *sql.DB, MobileNo string) (Tutor, string) {
-	query := fmt.Sprintf("SELECT * FROM Tutor WHERE MobileNo = '%s'", MobileNo)
+func TutorLogin(db *sql.DB, TutorID string) (Tutor, string) {
+	query := fmt.Sprintf("SELECT * FROM Tutor WHERE TutorID = '%s'", TutorID)
 
 	results := db.QueryRow(query)
 	var errMsg string
 
-	switch err := results.Scan(&passengers.PassengerID, &passengers.FirstName, &passengers.LastName, &passengers.MobileNo, &passengers.EmailAdd); err {
+	switch err := results.Scan(&tutors.TutorID, &tutors.FirstName, &tutors.LastName, &tutors.MobileNo, &tutors.EmailAdd); err {
 	case sql.ErrNoRows:
-		errMsg = "Mobile number not found. Passenger login failed."
+		errMsg = "Mobile number not found. Tutor login failed."
 	case nil:
 	default:
 		panic(err.Error())
@@ -120,7 +122,7 @@ func TutorLogin(db *sql.DB, MobileNo string) (Tutor, string) {
 func tutor(w http.ResponseWriter, r *http.Request) {
 
 	if r.Header.Get("Content-type") == "application/json" {
-		// POST is for creating new passenger
+		// POST is for creating new tutor
 		if r.Method == "POST" {
 			// read the string sent to the service
 			var newTutor Tutor
@@ -129,64 +131,56 @@ func tutor(w http.ResponseWriter, r *http.Request) {
 			if err == nil {
 				// convert JSON to object
 				json.Unmarshal(reqBody, &newTutor)
-				//Check if user fill up the required information for registering Passenger's account
+				//Check if user fill up the required information for registering tutor's account
 				if newTutor.TutorID == "" || newTutor.Name == "" || newTutor.Description == "" {
 					w.WriteHeader(http.StatusUnprocessableEntity)
-					w.Write([]byte("422 - Please supply passenger " + "information " + "in JSON format"))
+					w.Write([]byte("422 - Please supply tutor " + "information " + "in JSON format"))
 					return
 				} else {
-					CreateNewTutor(db, newTutor) //Once everything is checked, passenger's account will be created
+					CreateNewTutor(db, newTutor) //Once everything is checked, tutor's account will be created
 					w.WriteHeader(http.StatusCreated)
-					w.Write([]byte("201 - Successfully created passenger's account"))
+					w.Write([]byte("201 - Successfully created tutor's account"))
 				}
 			} else {
 				w.WriteHeader(
 					http.StatusUnprocessableEntity)
-				w.Write([]byte("422 - Please supply passenger information " +
+				w.Write([]byte("422 - Please supply tutor information " +
 					"in JSON format"))
 			}
 		}
-		//---PUT is for creating or updating existing passenger---
+		//---PUT is for creating or updating existing tutor---
 		if r.Method == "PUT" {
-			queryParams := r.URL.Query() //used to resolve the conflict of calling API using the '%s'?PassengerID='%s' method
+			queryParams := r.URL.Query() //used to resolve the conflict of calling API using the '%s'?TutorID='%s' method
 			TutorID = queryParams["TutorID"][0]
 			reqBody, err := ioutil.ReadAll(r.Body)
 			if err == nil {
 				json.Unmarshal(reqBody, &tutors)
-				//Check if user fill up the required information for updating Passenger's account information
+				//Check if user fill up the required information for updating tutor's account information
 				if tutors.TutorID == "" || tutors.Name == "" || tutors.Description == "" {
 					w.WriteHeader(http.StatusUnprocessableEntity)
-					w.Write([]byte("422 - Please supply passenger " + " information " + "in JSON format"))
+					w.Write([]byte("422 - Please supply tutor " + " information " + "in JSON format"))
 				} else {
 					tutors.TutorID = TutorID
 					UpdateTutor(db, tutors)
 					w.WriteHeader(http.StatusAccepted)
-					w.Write([]byte("202 - Successfully updated passenger's information"))
+					w.Write([]byte("202 - Successfully updated tutor's information"))
 				}
 			} else {
 				w.WriteHeader(
 					http.StatusUnprocessableEntity)
-				w.Write([]byte("422 - Please supply " + "passenger information " + "in JSON format"))
+				w.Write([]byte("422 - Please supply " + "tutor information " + "in JSON format"))
 			}
 		}
 
 	}
-	if r.Method == "GET" {
-		if _, ok := tutors[params["TutorID"]]; ok {
-			json.NewEncoder(w).Encode(
-				tutors[params["TutorID"]])
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("404 - No tutor found"))
-		}
-	}
-	//---Deny any deletion of passenger's account or other passenger's information
+	//---Deny any deletion of tutor's account or other tutor's information
 	if r.Method == "DELETE" {
 		w.WriteHeader(http.StatusAccepted)
-		w.Write([]byte("403 - For audit purposes, passenger's account cannot be deleted."))
+		w.Write([]byte("403 - For audit purposes, tutor's account cannot be deleted."))
 	}
 }
 
+/*
 func listtutor(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		if _, ok := tutors[params["TutorID"]]; ok {
@@ -211,10 +205,11 @@ func searchtutor(w http.ResponseWriter, r *http.Request) {
 
 	}
 }
+*/
 
 //Function main for testing purposes only
 func main() {
-	// instantiate passengers
+	// instantiate tutor
 	tutors, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/tutors")
 
 	db = tutors
@@ -222,16 +217,16 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	//handle the API connection across all three microservices, Passengers, Trips and Drivers
+	//handle the API connection across tutor's microservices
 	router := mux.NewRouter()
 	router.HandleFunc("/tutors", tutor).Methods(
 		"GET", "POST", "PUT", "DELETE")
-	router.HandleFunc("/tutors/listtutors", listtutor).Methods(
+	/*router.HandleFunc("/tutors/listtutors", listtutor).Methods(
 		"GET")
 	router.HandleFunc("/tutors/searchtutors", searchtutor).Methods(
-		"GET")
-	fmt.Println("Tutors microservice API --> Listening at port 5000")
-	log.Fatal(http.ListenAndServe(":5000", router))
+		"GET")*/
+	fmt.Println("Tutors microservice API --> Listening at port 8011")
+	log.Fatal(http.ListenAndServe(":8011", router))
 
 	defer db.Close()
 }
